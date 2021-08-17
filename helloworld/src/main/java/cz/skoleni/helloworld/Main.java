c6d3d352f023af62b00e1332fdada6f625cca0d0
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-08-17 13:27:41 +0200

    logovani pomoci Logback

--- helloworld/src/main/java/cz/skoleni/helloworld/Main.java
+++ helloworld/src/main/java/cz/skoleni/helloworld/Main.java
@@ -1,9 +1,17 @@
 package cz.skoleni.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World v2");
+        log.debug("debug hlaska");
+        log.info("info hlaska");
+        log.warn("warn hlaska");
+        log.error("error hlaska");
     }
 
 }
