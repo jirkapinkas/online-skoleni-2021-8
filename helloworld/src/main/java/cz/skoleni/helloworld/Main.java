b84242ba978d9d054ddea6cf532b59c9fec9f132
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-08-17 14:26:40 +0200

    logovani pomoci Logback II

--- helloworld/src/main/java/cz/skoleni/helloworld/Main.java
+++ helloworld/src/main/java/cz/skoleni/helloworld/Main.java
@@ -3,15 +3,28 @@ package cz.skoleni.helloworld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
     static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         log.debug("debug hlaska");
         log.info("info hlaska");
         log.warn("warn hlaska");
         log.error("error hlaska");
+
+//        String fileName = "pom.xml2";
+//        try {
+//            List<String> strings = Files.readAllLines(Paths.get(fileName));
+//            strings.forEach(System.out::println);
+//        } catch (Exception e) {
+//            log.error("Error reading file: " + fileName, e);
+//        }
     }
 
 }
