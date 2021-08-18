17ddf8076b8564cfa32e50b24a7de52e41b86ee5
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-08-18 12:57:54 +0200

    equals + hashCode

--- helloworld/src/main/java/cz/skoleni/helloworld/Dog.java
+++ helloworld/src/main/java/cz/skoleni/helloworld/Dog.java
@@ -1,5 +1,7 @@
 package cz.skoleni.helloworld;
 
+import java.util.Objects;
+
 public class Dog {
 
     private String name;
@@ -21,5 +23,18 @@ public class Dog {
         return "Dog{" +
                 "name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
