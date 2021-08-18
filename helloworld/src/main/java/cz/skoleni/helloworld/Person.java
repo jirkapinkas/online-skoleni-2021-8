6210b339f7ff6f89f73466f617298d4f284c1102
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-08-18 12:28:27 +0200

    Priklad na Builder Design Pattern

--- helloworld/src/main/java/cz/skoleni/helloworld/Person.java
+++ helloworld/src/main/java/cz/skoleni/helloworld/Person.java
@@ -13,5 +13,50 @@ public class Person {
     private boolean alive;
 
     private int age;
+//
+//    public static PersonBuilder builder() {
+//        return new PersonBuilder();
+//    }
+//
+//    public static class PersonBuilder {
+//
+//        private String name;
+//
+//        private long id;
+//
+//        private boolean alive;
+//
+//        private int age;
+//
+//        public PersonBuilder name(String name) {
+//            this.name = name;
+//            return this;
+//        }
+//
+//        public PersonBuilder alive(boolean alive) {
+//            this.alive = alive;
+//            return this;
+//        }
+//
+//        public PersonBuilder id(long id) {
+//            this.id = id;
+//            return this;
+//        }
+//
+//        public PersonBuilder age(int age) {
+//            this.age = age;
+//            return this;
+//        }
+//
+//        public Person build() {
+//            Person person = new Person();
+//            person.setName(this.name);
+//            person.setAlive(this.alive);
+//            person.setId(this.id);
+//            person.setAge(this.age);
+//            return person;
+//        }
+//
+//    }
 
 }
