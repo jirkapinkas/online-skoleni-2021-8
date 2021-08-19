39fa72828b96b701537a883b5f553d07c1d668ae
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-08-19 13:37:12 +0200

    Streamovani :-)

--- helloworld/src/main/java/cz/skoleni/helloworld/MainIntStream.java
+++ helloworld/src/main/java/cz/skoleni/helloworld/MainIntStream.java
@@ -2,6 +2,8 @@ package cz.skoleni.helloworld;
 
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class MainIntStream {
 
@@ -9,12 +11,19 @@ public class MainIntStream {
         List<Integer> integers = List.of(1,2,3,4,5,0,2,5,4,3,10,2,1,5);
 
         OptionalInt minOptional = integers.stream()
-                .mapToInt(e -> e)
+                .mapToInt(Integer::valueOf)
+//                .mapToInt(e -> e)
                 .min();
         if(minOptional.isPresent()) {
             System.out.println("Minimum = " + minOptional.getAsInt());
         } else {
             System.out.println("Minimum nebylo nalezeno");
         }
+
+        List<String> strings = List.of("1","2","3");
+        strings.stream()
+                .mapToInt(Integer::valueOf)
+                .forEach(System.out::println);
     }
 }
+
